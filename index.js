0f9140b817bbae62d502a42fbb1e485bdf56350e
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-07-05 06:02:55 +0545

    second

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express =require('express');
-// requries for the import
-const app=express();
-// app listen incomimg request
-app.get('/', (req,res)=>{
-  res.send({hi:'there'})
-} )
-app.listen(5000);
+const express =require('express');
+// requries for the import
+const app=express();
+// app listen incomimg request
+
+app.get('/', (req,res)=>{
+  res.send({hey:'bye'})
+} )
