3aca41f22a9d502ead497758963152c37bd520a9
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-08-15 12:14:35 +0545

    github

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,8 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  githubClientID:process.env.GITHUB_ID,
+githubClientSecret:process.env.GITHUB_SEC,
 };
 
