2569c48205bdef564c7e83745329381600b1e8b1
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-08-15 13:00:06 +0545

    khg

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
-require('./services/passport');
+require('./services/Passport');
 
 mongoose.connect(keys.mongoURI);
 
