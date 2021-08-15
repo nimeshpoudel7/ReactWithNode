6d0c5ccc9dc1d0e90e32d951b3ee4e2f272b49db
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-08-15 12:28:39 +0545

    commit

--- config/Dev.js
+++ config/Dev.js
@@ -3,6 +3,8 @@
 module.exports ={
 googleClientID:'493824631047-s4njff2fn4t03s2tkh6bbhnsp68hofk2.apps.googleusercontent.com',
 googleClientSecret:'TKR82JFYEcAJKFB1VYUd8QH4',
+githubClientID:'0c3310860da167e58770',
+githubClientSecret:'69c85e374c355518e7d54b8effc1c5099c9ac332',
 mongoURI:'mongodb+srv://emailbd:Pd8RYAsANtNPgGmB@emaily.pwqvx.mongodb.net/nimesh?retryWrites=true&w=majority',
 cookieKey:'asdfghjklilejmafjnajfnajf'
 }
