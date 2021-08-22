c987068a2791a995a8861f9056b75479411ea139
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-08-22 16:45:52 +0545

    fornend connect

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,8 @@
-//keys.js figure out what set of cerdential to return
-if (process.env.NODE_ENV === "production"){
-// production
-module.exports=require('./prod');
-
-}else{
-    //developmetn
-    //suruma import and export
-    module.exports = require('./dev');
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
 }
